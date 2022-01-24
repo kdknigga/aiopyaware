5ab439cd31eec532d0f78e98f6f09726d71e1719
Kris Knigga <kris@knigga.com>
2022-01-24 11:17:11 -0600

    Move loop to main so either output mode can be looped

--- example.py
+++ example.py
@@ -7,24 +7,44 @@ import logging
 from pprint import pprint
 from aiopyaware.piaware import PiAware
 
-locale.setlocale(locale.LC_ALL, '')
+locale.setlocale(locale.LC_ALL, "")
 
-async def main(closest=None):
+
+async def main(closest=None, refresh=None):
     async with aiohttp.ClientSession() as session:
         pa = PiAware("http://piaware", session)
+        await pa.get_receivers()
 
-        if closest:
-            await show_closest(pa, closest)
+        r_refresh = max([r.refresh_interval for r in pa.receivers.values()])/1000
+
+        if refresh:
+            if r_refresh > refresh:
+                refresh = r_refresh
+                logging.warning(
+                    f"Overriding refresh interval setting with larger value provided by the PiAware: {refresh}"
+                )
+
+            while True:
+                if closest:
+                    await show_closest(pa, closest)
+                else:
+                    await dump_status(pa)
+
+                print("")
+                await asyncio.sleep(refresh)
         else:
-            await dump_status(pa)
+            if closest:
+                await show_closest(pa, closest)
+            else:
+                await dump_status(pa)
 
 
 def _print_ac_list(aircraft_list: list) -> None:
     for ac in aircraft_list:
-        distance = ac.get('distance')
-        alt = ac.get('alt_baro')
-        gs = ac.get('gs')
-        track = ac.get('track')
+        distance = ac.get("distance")
+        alt = ac.get("alt_baro")
+        gs = ac.get("gs")
+        track = ac.get("track")
 
         text = f"callsign: {ac.get('flight')} "
 
@@ -42,12 +62,12 @@ def _print_ac_list(aircraft_list: list) -> None:
             text += f"groundspeed: {ac.get('gs')}kt "
         else:
             text += f"groundspeed: Unknown "
-        
+
         if track:
             text += f"track: {ac.get('track')}° "
         else:
             text += f"track: Unknown "
-        
+
         print(f"    - {ac['hex']}")
         print(f"       {text}")
 
@@ -61,29 +81,16 @@ def _filter_aircraft(pa: PiAware, seen: int = 60) -> dict:
 
 
 async def show_closest(pa: PiAware, closest: int):
-    await pa.get_receivers()
     await pa.get_aircraft()
+    sorted_aircraft = sorted(_filter_aircraft(pa), key=lambda i: i["distance"])
 
-    refresh = 5
-    #refresh = max([r.refresh_interval for r in pa.receivers.values()])
-
-    while True:
-        await pa.get_aircraft()
-        sorted_aircraft = sorted(_filter_aircraft(pa), key=lambda i: i["distance"])
-
-        _print_ac_list(sorted_aircraft[:closest])
-        print("")
-
-        await asyncio.sleep(refresh)
+    _print_ac_list(sorted_aircraft[:closest])
 
 
 async def dump_status(pa: PiAware):
     await pa.update_status()
-    await pa.get_receivers()
     await pa.update_receivers()
     await pa.get_aircraft()
-
-    # pprint(pa.status)
 
     status = None
 
@@ -108,6 +115,12 @@ parser.add_argument(
     "-c", "--closest", help="Display the X closest aircraft to the receiver", type=int
 )
 parser.add_argument(
+    "-i",
+    "--refresh-interval",
+    help="The time to wait between refreshes.  If this number is smaller than the refresh interval provided by the PiAware, the larger interval will be used.",
+    type=int,
+)
+parser.add_argument(
     "-l",
     "--log-level",
     help="Amount of logging desired.  (default: %(default)s)",
@@ -126,6 +139,6 @@ log_level_map = {
 logging.basicConfig(level=log_level_map[args.log_level])
 
 try:
-    asyncio.run(main(closest=args.closest))
+    asyncio.run(main(closest=args.closest, refresh=args.refresh_interval))
 except KeyboardInterrupt:
     pass
